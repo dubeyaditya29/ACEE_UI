1edcde53bdd5a8de43a92cd7374d5384aaffe804
aditya dubey <48158220+dubeyaditya29@users.noreply.github.com>
2022-02-17 03:54:52 +0530

    fix: The code can now handle multiple passwords checks

--- Scripts/see-pass.js
+++ Scripts/see-pass.js
@@ -1,8 +1,14 @@
-const content = document.querySelector("#inp-password");
-const seePassword = document.querySelector("#pass-icon");
+const content = document.querySelector(".inp-password");
+const seePassword = document.querySelector(".container");
 
-seePassword.addEventListener("click", () => {
-  const type =
-    content.getAttribute("type") === "password" ? "text" : "password";
-  content.setAttribute("type", type);
-});
+seePassword.addEventListener(
+  "click",
+  (e) => {
+    // Have used the concept of Event Delegation..
+    var ele = e.target.parentNode.parentElement.firstElementChild;
+    var type = ele.getAttribute("type") === "password" ? "text" : "password";
+    ele.setAttribute("type", type);
+    //console.log(e);
+  },
+  false
+);
